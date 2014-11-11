a49b7b216c544a45bd607dc4ce98a899dcb97b2c
Elijah <kimelijah90@gmail.com>
2014-11-11 15:08:39 -0500

    created categories in a single migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141111192146) do
+ActiveRecord::Schema.define(version: 20141111144912) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,11 +21,6 @@ ActiveRecord::Schema.define(version: 20141111192146) do
     t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
-  end
-
-  create_table "categorys", force: true do |t|
-    t.string "name"
-    t.string "description"
   end
 
   create_table "posts", force: true do |t|
