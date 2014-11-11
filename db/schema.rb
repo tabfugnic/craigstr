731cd6fc95b82fc21bd572646d3f282417c1608c
Elijah <kimelijah90@gmail.com>
2014-11-11 15:30:56 -0500

    fixing migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141111144806) do
+ActiveRecord::Schema.define(version: 20141111144912) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 20141111144806) do
   end
 
   create_table "posts", force: true do |t|
-    t.string   "title"
+    t.string   "title",       null: false
     t.text     "description"
     t.string   "image"
     t.integer  "user_id"
