1162691ef8615d6d8dbf12ebfa5f26d7dffc7c27
Elijah <kimelijah90@gmail.com>
2014-11-11 15:17:29 -0500

    Combined category migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141111144912) do
+ActiveRecord::Schema.define(version: 20141111144806) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 20141111144912) do
   end
 
   create_table "posts", force: true do |t|
-    t.string   "title",       null: false
+    t.string   "title"
     t.text     "description"
     t.string   "image"
     t.integer  "user_id"
