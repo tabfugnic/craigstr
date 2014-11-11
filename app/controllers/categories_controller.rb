fd0e83282c8d9e021662d848b294a2092635f7e9
Hayley <hayleyanderson@zoho.com>
2014-11-11 16:47:59 -0500

    Change links in ALL the places
    * Previously categories was not under regions in the routes b/c branches
    * Now that they are, all the links involving categories have to change
    * Now hopefully all of the links are correct and working

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,41 +1,43 @@
 class CategoriesController < ApplicationController
-  before_action :check_admin, except: [:index, :show]
-
-  def index
-    @categories = Category.all
-  end
+  before_action :check_admin, except: :show
 
   def new
+    @region = Region.find(params[:region_id])
     @category = Category.new
   end
 
   def create
+    @region = Region.find(params[:region_id])
     @category = Category.new(category_params)
     @category.save
 
-    redirect_to @category
+    redirect_to [@region, @category]
   end
 
   def show
+    @region = Region.find(params[:region_id])
     @category = find_category
   end
 
   def edit
+    @region = Region.find(params[:region_id])
     @category = find_category
   end
 
   def update
+    @region = Region.find(params[:region_id])
     @category = find_category
     @category.update(category_params)
 
-    redirect_to @category
+    redirect_to [@region, @category]
   end
 
   def destroy
+    @region = Region.find(params[:region_id])
     @category = find_category
     @category.delete
 
-    redirect_to categories_path
+    redirect_to @region
   end
 
   private
