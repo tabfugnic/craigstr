66c01e41e38caa88a5e9f8679332f4c2b074a8ac
Hayley <hayleyanderson@zoho.com>
2014-11-11 10:01:25 -0500

    Add dependent: :destroy to posts in user model
    * We do not want post orphans if a user closes their account

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: true
   validates :password_digest, presence: true
 
-  has_many :posts
+  has_many :posts, dependent: :destroy
 end
