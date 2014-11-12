130efe4fa9974f54a162ff73497729149a2ebcde
Hayley <hayleyanderson@zoho.com>
2014-11-12 16:45:09 -0500

    Add category relationships to posts and categories
    * Add has_many category relationships to posts and categories models
    * Add has_many categories/posts through category relationships

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,9 @@ class Post < ActiveRecord::Base
   belongs_to :user
   belongs_to :region
 
+  has_many :category_relationships, dependent: :destroy
+  has_many :categories, through: :category_relationships
+
   def owned_by?(user)
     self.user == user
   end
