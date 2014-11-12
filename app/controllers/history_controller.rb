845859e55be69c052aa395c85e0c6deb7ab44641
Hayley <hayleyanderson@zoho.com>
2014-11-12 11:32:53 -0500

    Move functionality of history to user show
    * Remove history to make more RESTful and whatever
    * Move functionality to user show
    * Change routes, links accordingly

--- app/controllers/history_controller.rb
+++
@@ -1,7 +0,0 @@
-class HistoryController < ApplicationController
-  before_action :require_login
-
-  def show
-    @posts = current_user.posts
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,9 @@ require "monban/constraints/signed_out"
 Rails.application.routes.draw do
 
   resource :session, only: [:new, :create, :destroy]
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :show]
 
   get "admin", to: "admin#show", as: :admin
-  resource :history, only: [:show]
   resources :posts, except: :index
   resources :regions, except: [:update, :destroy] do
     resources :categories, except: [:index]
