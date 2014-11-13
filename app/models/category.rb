5eab9202376081ec0a799afbd52772dc7b161f7d
Hayley <hayleyanderson@zoho.com>
2014-11-13 14:48:17 -0500

    Simplify region-finding

--- app/models/category.rb
+++ app/models/category.rb
@@ -5,6 +5,6 @@ class Category < ActiveRecord::Base
   has_many :posts, through: :category_relationships
 
   def ordered_posts(region)
-    posts.where(region_id: region.id).order(created_at: :desc)
+    posts.where(region: region).order(created_at: :desc)
   end
 end
