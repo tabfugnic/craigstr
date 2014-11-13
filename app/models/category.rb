44bd56aefa65544d1419a8caba90c71be0df8f45
Hayley <hayleyanderson@zoho.com>
2014-11-13 13:57:44 -0500

    Take regions into account
    * Forgot regions was a thing
    * Pass region into ordered_posts method
    * Only get posts with correct region id

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,7 +4,7 @@ class Category < ActiveRecord::Base
   has_many :category_relationships, dependent: :destroy
   has_many :posts, through: :category_relationships
 
-  def ordered_posts
-    self.posts.order(created_at: :desc)
+  def ordered_posts(region)
+    self.posts.where(region_id: region.id).order(created_at: :desc)
   end
 end
